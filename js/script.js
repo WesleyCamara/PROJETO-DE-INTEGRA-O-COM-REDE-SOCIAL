19be680c2ffc60f78db4acc0393d3508e30778a5
Fabrício Cardoso <santoscardoso370@gmail.com>
2020-04-03 22:45:10 -0300

    Scroll na area de publicação com Javascript

--- js/script.js
+++ js/script.js
@@ -41,3 +41,44 @@ function removeMensagemErro(){
     alertaErro.classList.remove("ativo")
 }
 
+
+// Animação ao scroll dos textos
+
+const debounce = function(func, wait, immediate){
+    let timeout;
+    return function(...args){
+        const context = this;
+        const later = function(){
+            timeout = null;
+            if(!immediate)
+            func.apply(context,args);
+        };
+
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later,wait);
+        if(callNow)func.apply(context,args);
+    };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.85);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll',debounce(function(){
+        animeScroll();
+    }, 100));
+}
